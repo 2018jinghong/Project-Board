c780860d111c11d5e19c6c731645550e9cafdcdb
陈力 <cl137046952@outlook.com>
2018-10-31 09:13:55 +0800

    fix php

--- board/postNew.php
+++ board/postNew.php
@@ -4,11 +4,11 @@ function check($words){
     require_once 'aiSDK/AipImageCensor.php';
 
     // 你的 APPID AK SK
-    const APP_ID = '14353274';
-    const API_KEY = 'wtDLUNvcuTfAGygEqGDc9PMQ';
-    const SECRET_KEY = 'm5KeOPj9feTbCxDpYCsaXW7BjCxYPYoq';
+    $APP_ID = '14353274';
+    $API_KEY = 'wtDLUNvcuTfAGygEqGDc9PMQ';
+    $SECRET_KEY = 'm5KeOPj9feTbCxDpYCsaXW7BjCxYPYoq';
     
-    $client = new AipImageCensor(APP_ID, API_KEY, SECRET_KEY);
+    $client = new AipImageCensor($APP_ID, $API_KEY, $SECRET_KEY);
     $result =$client->antiSpam($words);
     return  $result['result']['spam'];
 
